d25a4bc5ce817cde44eb055ed67821bfed1e961d
ygritte00 <159973006+ygritte00@users.noreply.github.com>
2024-02-20 21:36:20 -0600

    Add files via upload

---
+++ docs/next.php
@@ -0,0 +1,41 @@
+<html>
+<head></head>
+<body>
+<h1>12662. C16 </h1>
+
+<h2>Descripción</h2>
+OMIJal se ha encontrado con una variante en el Concurso ACM, pues el numero de programadores que conforman un equipo puede variar, entonces modifica tu programa anterior pues ahora deberás preguntar el numero de 3 integrantes por equipo, al final indica cuantos equipos puedenformarse y cuantos programadores quedan fuera
+<h2>Entrada</h2>
+Dos números que representen el número de programadores y el número de programadores por equipo
+
+<h2>Salida</h2>
+Salida con el número de equipos formados y los estudiantes sin equipo, escrito exactamente como en el ejemplo
+<h2>Ejemplo</h2>
+<table>
+<tr>
+    <th>Entrada</th>
+    <th>Salida</th>
+    <tr></tr>
+    <th>5<br>4</th>
+    <th>Equipos formados: 1 <br> Estudiantes sin equipo: 1</th>
+    <tr></tr>
+    <th>18<br>3</th>
+    <th>Equipos formados: 6 <br> Estudiantes sin equipo: 6</th>
+</tr>
+
+</table>
+
+<?php
+$a=9;
+$b=3;
+$c=$b/$b;
+$d=a-c;
+
+$m=($a+$b+$c)/3;
+echo "Variables a, b  ", $a, " ", $b, "<br>";
+echo "Prom: ", $m;
+?>
+
+
+</body>
+</html>
